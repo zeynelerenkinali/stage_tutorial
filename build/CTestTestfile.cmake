7ec1bf5daed2fa64944fefc7468fe045bd1e7d78
zeynelerenkinali <ernknal@gmail.com>
2024-08-05 13:31:12 +0100

    First repo for simulation_stage_tutorial

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/eren/simulation_ws/src
+# Build directory: /home/eren/simulation_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("stage_tutorial")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/eren/simulation_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/eren/simulation_ws/devel;/home/eren/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/eren/simulation_ws/devel;/home/eren/catkin_ws/devel;/opt/ros/noetic' != '' else []
