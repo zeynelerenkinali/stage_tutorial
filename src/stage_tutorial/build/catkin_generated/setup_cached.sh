90d6df465b1eeb9ec8896c63299e325da812125e
zeynelerenkinali <ernknal@gmail.com>
2024-08-06 13:51:25 +0100

    launch file added

---
+++ src/stage_tutorial/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/eren/simulation_ws/src/stage_tutorial/build/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/eren/simulation_ws/src/stage_tutorial/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/eren/simulation_ws/src/stage_tutorial/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/eren/simulation_ws/src/stage_tutorial:$ROS_PACKAGE_PATH"
